29a483d89b5264d152dbcadd4cc6bce8859dbbc9
Ren Lawrence Lin <70042943+KaitoTLex@users.noreply.github.com>
2024-09-04 15:58:40 -0700

    Update lsps.nix

--- config/plugins/lsps.nix
+++ config/plugins/lsps.nix
@@ -1,21 +1,26 @@
 # LSP setup
 { pkgs, inputs, ... }:
 {
-  plugins = {
-    lsp = {
-      enable = true;
-      inlayHints = true;
-      servers = {
-        bashls.enable = true;
-        clangd.enable = true;
-        pyright.enable = true;
-        nixd.enable = true;
-        nushell.enable = true;
-        svelte.enable = true;
-        tailwindcss.enable = true;
-        typst-lsp.enable = true;
-        marksman.enable = true;
-      };
+  extraPackages = with pkgs; [
+    rust-analyzer
+    lua-language-server
+    texpresso
+    vim-julia-cell
+  ];
+
+  plugins.lsp = {
+    enable = true;
+    inlayHints = true;
+    servers = {
+      bashls.enable = true;
+      clangd.enable = true;
+      pyright.enable = true;
+      nixd.enable = true;
+      nushell.enable = true;
+      svelte.enable = true;
+      tailwindcss.enable = true;
+      # typst-lsp.enable = true;
+      marksman.enable = true;
     };
     typescript-tools.enable = true;
     lsp-lines.enable = true;
